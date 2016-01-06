902d705e32966252275c67a2298dbaf9ac32cca0
Daniel Olson <dolson@mcs.anl.gov>
2016-01-06 14:26:38 -0600

    initial commit for Base SEEDtk KBase SDK Module

---
+++ refresh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/SEEDtk/seedtk.git SEEDtk
+cd SEEDtk
+./seedtk-setup
+source user-env.sh
+mkdir Data
+Config --dirs --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub Data
+cd Data/Global
+wget -q -nH --cut-dirs=100 -m "ftp://ftp.kbase.us/SEEDtk/Global/"
+cd ../../Data/DnaRepo
+wget -q -nH --cut-dirs=100 -m "ftp://ftp.kbase.us/SEEDtk/DnaRepo/"
