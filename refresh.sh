9ca116c6a5581e0a4b4d8c3a753f34447da73373
Bruce Parrello <bparrello@figresearch.com>
2016-01-13 13:10:12 -0600

    New strategy.

--- refresh.sh
+++ refresh.sh
@@ -7,13 +7,11 @@ else
     ./seedtk-setup
     source user-env.sh
     mkdir Data
-    Config --dirs --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub Data
+    Config --dirs --dna=no --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub --kbase=../lib/SEEDtk/FIG_Config.pm Data
     cd Data/Global
     curl -O kmer_db.json "ftp://ftp.kbase.us/SEEDtk/Global/kmer_db.json"
     curl -O peg_md5_cdd.tbl "ftp://ftp.kbase.us/SEEDtk/Global/peg_md5_cdd.tbl"
     curl -O roles_cdd.tbl "ftp://ftp.kbase.us/SEEDtk/Global/roles_cdd.tbl"
     curl -O rep_roles.tbl "ftp://ftp.kbase.us/SEEDtk/Global/rep_roles.tbl"
     curl -O uni_roles.tbl "ftp://ftp.kbase.us/SEEDtk/Global/uni_roles.tbl"
-    cd ../../Data/DnaRepo  
-    # wget -q -nH --cut-dirs=100 -m "ftp://ftp.kbase.us/SEEDtk/DnaRepo/"
 fi
