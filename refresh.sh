9ffdb48b721b3b414c6ec3f391cadcd66703c8b0
Bruce Parrello <bparrello@figresearch.com>
2016-01-13 13:26:37 -0600

    Added more progress visibility to refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -9,9 +9,11 @@ else
     mkdir Data
     Config --dirs --dna=no --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub --kbase=../lib/SEEDtk/FIG_Config.pm Data
     cd Data/Global
+    echo Downloading global data files.
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/kmer_db.json"
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/peg_md5_cdd.tbl"
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/roles_cdd.tbl"
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/rep_roles.tbl"
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/uni_roles.tbl"
+    echo Global data files downloaded.
 fi
