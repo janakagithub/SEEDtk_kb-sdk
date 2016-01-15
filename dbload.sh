06c709d025401cb24a1ce22e9d0f2ca9fb5ba652
Bruce Parrello <bparrello@figresearch.com>
2016-01-15 17:58:49 -0600

    Added load script.

---
+++ dbload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd SEEDtk/Data
+echo Downloading load tar file.
+curl -sO "ftp://ftp.kbase.us/SEEDtk/reprepo.tar.gz"
+ShrubLoad --clear --exclusive --store --tar=reprepo.tar.gz
+echo Cleaning up.
+rm reprepo.tar.gz
+rm -rf Inputs/GenomeData
+rm -rf Inputs/SubsystemData
