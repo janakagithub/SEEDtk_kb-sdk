d8595482057314ca9b94e60ecad59a56188c95da
Bruce Parrello <bparrello@figresearch.com>
2016-01-16 03:38:59 -0600

    Cleanup fix for load.

--- dbload.sh
+++ dbload.sh
@@ -6,5 +6,6 @@ curl -sO "ftp://ftp.kbase.us/SEEDtk/reprepo.tar.gz"
 ShrubLoad --clear --exclusive --store --tar=reprepo.tar.gz
 echo Cleaning up.
 rm reprepo.tar.gz
-rm -rf Inputs/GenomeData
-rm -rf Inputs/SubsystemData
+rm -rf Inputs/GenomeData/*
+rm -rf Inputs/SubsystemData/*
+rm -rf LoadFiles/*
