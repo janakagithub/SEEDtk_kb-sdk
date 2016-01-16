ca5fe7099a1ece0102ef471530e0b817f85b685f
Bruce Parrello <bparrello@figresearch.com>
2016-01-16 11:04:10 -0600

    Smarter dbload

--- dbload.sh
+++ dbload.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# Use "force" to force downloading the tar file.
+
 source SEEDtk/user-env.sh
 cd SEEDtk/Data
-echo Downloading load tar file.
-curl -sO "ftp://ftp.kbase.us/SEEDtk/reprepo.tar.gz"
+if [ ! -e reprepo.tar.gz ] || [ $1 == "force" ]; then
+    echo Downloading load tar file.
+    curl -sO "ftp://ftp.kbase.us/SEEDtk/reprepo.tar.gz"
+fi
 ShrubLoad --clear --exclusive --store --tar=reprepo.tar.gz
 echo Cleaning up.
 rm reprepo.tar.gz
