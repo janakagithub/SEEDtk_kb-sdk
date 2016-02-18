f4c32264b3f383bb0e38a15041316148a5274e0e
janakagithub <janaka.e@gmail.com>
2016-02-17 18:08:49 -0600

    Update refresh.sh
    fixing path issues

--- refresh.sh
+++ refresh.sh
@@ -7,7 +7,7 @@ else
     ./seedtk-setup
     source user-env.sh
     mkdir Data
-    Config --dirs --dna=none --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub --kbase=../lib/FIG_Config.pm Data
+    perl Config.pl --dirs --dna=none --dbhost=db3.chicago.kbase.us --dbpass=when26crazy --dbuser=seedtk --dbname=seedtk_shrub --kbase=../lib/FIG_Config.pm Data
     cd Data/Global
     echo Downloading global data files.
     curl -sO "ftp://ftp.kbase.us/SEEDtk/Global/kmer_db.json"
